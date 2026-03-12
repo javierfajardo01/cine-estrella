dca85878ea548e35faad611a09d0ecff093b0712
javierfajardo01 <javitabara@hotmail.es>
2026-03-12 11:06:31 +0100

    Primer commit proyecto fin curso cine-estrella

---
+++ src/main/java/com/cine/CineEstrellaApplication.java
@@ -0,0 +1,13 @@
+package com.cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineEstrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineEstrellaApplication.class, args);
+	}
+
+}
